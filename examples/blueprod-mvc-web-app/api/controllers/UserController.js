9630ece95e73aa407b1e5cb9e3e9e05ce6dc066d
locnt <locnguyen140297@gmail.com>
2019-07-22 15:36:20 +0700

    [open api] create new user

--- examples/blueprod-mvc-web-app/api/controllers/UserController.js
+++ examples/blueprod-mvc-web-app/api/controllers/UserController.js
@@ -61,15 +61,115 @@ UserController.prototype.getUsers = async function (ctx, next) {
 
 /**
  * @swagger
+ * components:
+ *   requestBodies:
+ *     createdUser:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/newUserTemplate'
+ *         application/xml:
+ *           schema:
+ *             $ref: '#/components/schemas/newUserTemplate'
+ *   schemas:
+ *     newUserTemplate:
+ *       type: object
+ *       description: model for one user
+ *       required: [username, email, firstName, lastName, password]
+ *       properties:
+ *         username:
+ *           type: string
+ *         email:
+ *           type: string
+ *         firstName:
+ *           type: string
+ *         lastName:
+ *           type: string
+ *         birthday:
+ *           type: string
+ *           format: date
+ *         gender:
+ *           enum: [male, female]
+ *         nationality:
+ *           type: string
+ *         lang:
+ *           type: string
+ *         companyCode:
+ *           type: string
+ *         companyId:
+ *           type: string
+ *         userType:
+ *           type: string
+ *         status:
+ *           type: string
+ *         isAdmin:
+ *           type: boolean
+ *         password:
+ *           type: string
+ *         phones:
+ *           type: string
+ *           pattern: '(\+84)+([0-9]{9,10})\b'
+ *         addresses:
+ *           type: string
+ *         emails:
+ *           type: array
+ *           items:
+ *             type: object
+ *         metadata:
+ *           type: object
+ *         images:
+ *           type: string
+ *         aam:
+ *           type: number
+ *         location:
+ *           type: string
+ *         roles:
+ *           type: array
+ *           items:
+ *             type: object
+ *         _custom:
+ *           type: object
+ *         tz:
+ *           type: string
+ *         mailLoginToUser:
+ *           type: string
+ *         randomPassword:
+ *           type: string
+ */
+
+/**
+ * @swagger
  * /users:
  *   post:
- *     summary: Returns users
- *     description: Returns users
+ *     summary: Create user
+ *     description: Create user
  *     tags:
  *       - Users
+ *     requestBody:
+ *       $ref: '#/components/requestBodies/createdUser'
  *     responses:
  *       200:
- *         description: users
+ *         description: Create user successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The user ID.
+ *                 username:
+ *                   type: string
+ *                   description: The user name.
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Permission denied
+ *       404:
+ *         description: Not found
+ *       5XX:
+ *         description: Unexpected error.
  */
 UserController.prototype.createUser = async function (ctx, next) {
   users.push(ctx.body);
